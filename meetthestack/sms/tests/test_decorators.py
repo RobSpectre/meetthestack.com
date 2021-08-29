7ba9825d2d858912dbf79561d930be94cf021e8b
Rob Spectre <rob@brooklynhacker.com>
2021-08-29 19:04:05 -0400

    1) Added urls for `sms` app. 2) Turned tests for `sms` app into a module. 3) Added `sms` urls to the main django app.

---
+++ meetthestack/sms/tests/test_decorators.py
@@ -0,0 +1,50 @@
+from django.test import override_settings
+
+from sms.decorators import twilio_view
+
+from .test_sms import TwilioTestCase
+
+
+@override_settings(DEBUG=False)
+class SmsDecoratorsTestCase(TwilioTestCase):
+    def test_not_get_or_post(self):
+        response = self.client.delete("/sms/",
+                                      HTTP_HOST="example.com")
+
+        self.assertEqual(response.status_code, 405)
+
+    def test_wrong_twilio_signature_post(self):
+        response = self.client.post("/sms/", params={"Body": "foo"},
+                                    HTTP_X_TWILIO_SIGNATURE="Xxx",
+                                    HTTP_HOST="example.com")
+        self.assertEqual(response.status_code, 403)
+
+    def test_wrong_twilio_signature_get(self):
+        response = self.client.get("/sms/?Body=foo",
+                                   HTTP_X_TWILIO_SIGNATURE="Xxx",
+                                   HTTP_HOST="example.com")
+        self.assertEqual(response.status_code, 403)
+
+    def test_missing_twilio_signature(self):
+        response = self.client.post("/sms/", params={"Body": "foo"},
+                                    HTTP_HOST="example.com")
+        self.assertEqual(response.status_code, 403)
+
+    def test_correct_twilio_signature(self):
+        response = self.client.sms("Test.")
+        self.assertEqual(response.status_code, 200)
+        self.assert_twiml(response)
+
+    def test_sim_in_from_field(self):
+        response = self.client.sms("Test.", from_="sim:DExxx")
+
+        self.assertEqual(response.status_code, 200)
+        self.assert_twiml(response)
+
+    @override_settings(DEBUG=True)
+    def test_non_twiml_response(self):
+        @twilio_view
+        def return_empty_list(request):
+            return request
+
+        self.assertEqual([], return_empty_list([]))
---
+++ meetthestack/sms/tests/test_sms.py
@@ -0,0 +1,82 @@
+from django.test import TestCase
+from django.test import Client
+from django.test import override_settings
+
+from twilio.request_validator import RequestValidator
+
+
+class TwilioTestClient(Client):
+    def sms(self, body, path="/sms/", to="+15558675309", from_="+15556667777",
+            extra_params=None):
+        params = {"MessageSid": "SMtesting",
+                  "AccountSid": "ACxxxxx",
+                  "To": to,
+                  "From": from_,
+                  "Body": body,
+                  "Direction": "inbound",
+                  "FromCity": "BROOKLYN",
+                  "FromState": "NY",
+                  "FromCountry": "US",
+                  "FromZip": "55555"}
+
+        if extra_params:
+            for k, v in extra_params.items():
+                params[k] = v
+
+        HTTP_HOST = "example.com"
+        validator = RequestValidator("yyyyyyyy")
+        absolute_url = "http://{0}{1}".format(HTTP_HOST,
+                                              path)
+        signature = validator.compute_signature(absolute_url,
+                                                params)
+
+        return self.post(path, params,
+                         HTTP_X_TWILIO_SIGNATURE=signature,
+                         HTTP_HOST=HTTP_HOST)
+
+    def call(self, to, path="/voice/", from_="+15556667777",
+             extra_params=None):
+        params = {"CallSid": "CAtesting",
+                  "AccountSid": "ACxxxxx",
+                  "To": to,
+                  "From": from_,
+                  "Direction": "inbound",
+                  "FromCity": "BROOKLYN",
+                  "FromState": "NY",
+                  "FromCountry": "US",
+                  "FromZip": "55555"}
+
+        if extra_params:
+            for k, v in extra_params.items():
+                params[k] = v
+
+        HTTP_HOST = "example.com"
+        validator = RequestValidator("yyyyyyyy")
+        absolute_url = "http://{0}{1}".format(HTTP_HOST,
+                                              path)
+        signature = validator.compute_signature(absolute_url,
+                                                params)
+
+        return self.post(path, params,
+                         HTTP_X_TWILIO_SIGNATURE=signature,
+                         HTTP_HOST=HTTP_HOST)
+
+
+@override_settings(TWILIO_AUTH_TOKEN="yyyyyyyy",
+                   ALLOWED_HOSTS=['example.com'])
+class TwilioTestCase(TestCase):
+    def setUp(self):
+        self.client = TwilioTestClient()
+
+    def assert_twiml(self, response):
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<Response")
+
+
+@override_settings(TWILIO_AUTH_TOKEN="yyyyyyyy",
+                   ALLOWED_HOSTS=['example.com'])
+class RouterTestCase(TwilioTestCase):
+    def test_sms_extra_parameters(self):
+        response = self.client.sms("Test.")
+
+        self.assert_twiml(response)
